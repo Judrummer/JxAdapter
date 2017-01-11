29a5c4a48819c07514c6d2ed9285516a9c83d080
judrummer <judrummercpe@gmail.com>
2017-01-12 00:38:07 +0700

    adddiffutil to rxjava

--- jxadapter-rxjava/src/main/java/com/github/judrummer/jxadapter_rxjava/JxAdapterRxJava.kt
+++ jxadapter-rxjava/src/main/java/com/github/judrummer/jxadapter_rxjava/JxAdapterRxJava.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.github.judrummer.jxadapter.JxAdapter
+import com.github.judrummer.jxadapter.JxDiffUtil
 import com.github.judrummer.jxadapter.JxViewHolder
 import rx.Observable
 import rx.Subscription
@@ -12,13 +13,13 @@ import rx.Subscription
  * Created by judrummer on 23/12/2559.
  */
 
-fun RecyclerView.rx_jxAdapter(observable: Observable<List<Any>>, vararg jxHolderList: JxViewHolder<*>): Subscription {
-    return rx_jxAdapter(observable, jxHolderList.toList())
+fun RecyclerView.rx_jxAdapter(observable: Observable<List<Any>>, vararg jxHolderList: JxViewHolder<*>, jxDiffUtil: JxDiffUtil? = null): Subscription {
+    return rx_jxAdapter(observable, jxHolderList.toList(), jxDiffUtil)
 }
 
 fun RecyclerView.rx_jxAdapter(observable: Observable<List<Any>>,
-                                jxHolderList: List<JxViewHolder<*>>): Subscription {
-    val jxAdapter = JxAdapter(jxHolderList)
+                              jxHolderList: List<JxViewHolder<*>>, jxDiffUtil: JxDiffUtil? = null): Subscription {
+    val jxAdapter = JxAdapter(jxHolderList, jxDiffUtil)
     adapter = jxAdapter
     return observable.subscribe {
         jxAdapter.items = it
