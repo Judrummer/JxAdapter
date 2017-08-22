e0967c0b7e2f9d9c44fd1b1f148cd28a448c17e4
judrummer <judrummercpe@gmail.com>
2017-08-22 23:35:36 +0700

    refactor to use cache but need to fix unittest

--- example/src/main/java/com/github/judrummer/example/SimpleActivity.kt
+++ example/src/main/java/com/github/judrummer/example/SimpleActivity.kt
@@ -3,39 +3,76 @@ package com.github.judrummer.example
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.ViewGroup
 import com.github.judrummer.jxadapter.JxAdapter
-import com.github.judrummer.jxadapter.JxDiffUtil
 import com.github.judrummer.jxadapter.JxViewHolder
 import com.taskworld.kxandroid.format
 import kotlinx.android.synthetic.main.activity_simple.*
-import kotlinx.android.synthetic.main.item_invoice_footer.view.*
-import kotlinx.android.synthetic.main.item_invoice_header.view.*
-import kotlinx.android.synthetic.main.item_invoice_item.view.*
+import kotlinx.android.synthetic.main.item_invoice_footer.*
+import kotlinx.android.synthetic.main.item_invoice_header.*
+import kotlinx.android.synthetic.main.item_invoice_item.*
+
+
+class InvoiceHeaderViewHolder(parent: ViewGroup) : JxViewHolder<InvoiceHeaderViewData>(parent, R.layout.item_invoice_header) {
+
+    override fun bind(item: InvoiceHeaderViewData) {
+        tvItemInvoiceHeaderNumber.text = item.invoiceNumber
+    }
+
+}
+
+class InvoiceItemViewHolder(parent: ViewGroup) : JxViewHolder<InvoiceItemViewData>(parent, R.layout.item_invoice_item) {
+
+    override fun bind(item: InvoiceItemViewData) {
+        tvItemInvoiceItemProductName.text = item.productName
+        tvItemInvoiceItemQuantity.text = "x ${item.quantity}"
+        tvItemInvoiceItemPrice.text = "$ ${item.price.format(2)}"
+        tvItemInvoiceItemTotal.text = "$ ${item.totalAmount.format(2)}"
+    }
+
+}
+
+class InvoiceFooterViewHolder(parent: ViewGroup) : JxViewHolder<InvoiceFooterViewData>(parent, R.layout.item_invoice_footer) {
+    override fun bind(item: InvoiceFooterViewData) {
+        tvItemInvoiceFooterTotal.text = "$ ${item.total.format(2)}"
+    }
+}
+
+class InvoiceSpaceViewHolder(parent: ViewGroup) : JxViewHolder<InvoiceSpaceViewData>(parent, R.layout.item_invoice_space) {
+    override fun bind(item: InvoiceSpaceViewData) {}
+}
 
 class SimpleActivity : AppCompatActivity() {
 
-    val jxAdapter = JxAdapter(
-            JxViewHolder<InvoiceHeaderViewData>(R.layout.item_invoice_header) { position, item ->
-                itemView.apply {
-                    tvItemInvoiceHeaderNumber.text = item.invoiceNumber
-                }
-            },
-            JxViewHolder<InvoiceItemViewData>(R.layout.item_invoice_item) { position, item ->
-                itemView.apply {
-                    tvItemInvoiceItemProductName.text = item.productName
-                    tvItemInvoiceItemQuantity.text = "x ${item.quantity}"
-                    tvItemInvoiceItemPrice.text = "$ ${item.price.format(2)}"
-                    tvItemInvoiceItemTotal.text = "$ ${item.totalAmount.format(2)}"
-                }
-            },
-            JxViewHolder<InvoiceFooterViewData>(R.layout.item_invoice_footer) { position, item ->
-                itemView.apply {
-                    tvItemInvoiceFooterTotal.text = "$ ${item.total.format(2)}"
-                }
-            },
-            JxViewHolder<InvoiceSpaceViewData>(R.layout.item_invoice_space) { position, item ->
+    private val jxAdapter = JxAdapter {
+        itemType(::InvoiceHeaderViewHolder)
+        itemType(::InvoiceItemViewHolder)
+        itemType(::InvoiceFooterViewHolder)
+        itemType(::InvoiceSpaceViewHolder)
+    }
 
-            })
+//    val jxAdapter = JxAdapter(
+//            JxViewHolder<InvoiceHeaderViewData>(R.layout.item_invoice_header) { position, item ->
+//                //                itemView.apply {
+////                    tvItemInvoiceHeaderNumber.text = item.invoiceNumber
+////                }
+//            },
+//            JxViewHolder<InvoiceItemViewData>(R.layout.item_invoice_item) { position, item ->
+//                //                itemView.apply {
+////                    tvItemInvoiceItemProductName.text = item.productName
+////                    tvItemInvoiceItemQuantity.text = "x ${item.quantity}"
+////                    tvItemInvoiceItemPrice.text = "$ ${item.price.format(2)}"
+////                    tvItemInvoiceItemTotal.text = "$ ${item.totalAmount.format(2)}"
+////                }
+//            },
+//            JxViewHolder<InvoiceFooterViewData>(R.layout.item_invoice_footer) { position, item ->
+//                //                itemView.apply {
+////                    tvItemInvoiceFooterTotal.text = "$ ${item.total.format(2)}"
+////                }
+//            },
+//            JxViewHolder<InvoiceSpaceViewData>(R.layout.item_invoice_space) { position, item ->
+//
+//            })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- jxadapter/src/main/java/com/github/judrummer/jxadapter/JxItem.kt
+++
@@ -1,7 +0,0 @@
-package com.github.judrummer.jxadapter
-
-/**
- * Created by judrummer on 24/3/2560.
- */
-
-interface JxItem
